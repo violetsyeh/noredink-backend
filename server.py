fcc4522083836089284c01224518a0803cd50228
vagrant <vagrant@vagrant.vm>
2018-08-16 19:21:38 +0000

    Boilerplate server.py to test data is seeded correctly and database is accurate/working

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import connect_to_db, db
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+    return "<html><body>Placeholder for the homepage.</body></html>"
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
